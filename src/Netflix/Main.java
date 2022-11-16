02d698efb5c94c1f865a0ed22dbad3bd196b9e0c
BrightonZhang1 <brighton.zhang23@ycdsbk12.ca>
2022-11-16 18:15:53 -0500

    Started set up for classes

---
+++ src/Netflix/Main.java
@@ -0,0 +1,13 @@
+package Netflix;
+
+import java.io.*;
+import Netflix.*;
+
+
+public class Main {
+    static BufferedReader keyboard;
+    public static void main (String[] args) throws IOException {
+        keyboard = new BufferedReader(new InputStreamReader(System.in));
+        //initalizing objects
+    }
+}
