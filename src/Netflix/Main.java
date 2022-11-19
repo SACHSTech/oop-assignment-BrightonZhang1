c114f378fab07ed9d0846501c5789ebc55d49c7c
BrightonZhang1 <brighton.zhang23@ycdsbk12.ca>
2022-11-19 15:10:31 -0500

    updated

--- src/Netflix/Main.java
+++ src/Netflix/Main.java
@@ -9,5 +9,10 @@ public class Main {
     public static void main (String[] args) throws IOException {
         keyboard = new BufferedReader(new InputStreamReader(System.in));
         //initalizing objects
+
+        Movie AvengersEndgame = new Movie("Avengers: Endgame", "Action, Adventure, Superhero", "2019", "Anthony Russo", 8.4, 182);
+        Movie Smile = new Movie("Smile", "Horror, Psychological Horror", "2022", "Parker Finn", 6.7, 115);
+        Movie Interstellar = new Movie("Interstellar", "Sci-Fi, Adventure", "2014", "Christopher Nolan", 8.6, 169);
+
     }
 }
